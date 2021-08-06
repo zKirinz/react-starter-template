84f908393aaf5b98e0545e4ebd6c42ef94ee043d
Trung Kien <kien123456k@gmail.com>
2021-08-06 16:42:08 +0700

    update welcome page

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,5 +1,28 @@
+import React from 'react'
+
+import './index.css'
+
 const App = () => {
-    return <h1>Welcome To My Template</h1>
+    return (
+        <React.Fragment>
+            <div id="stars"></div>
+            <div id="stars2"></div>
+            <div id="stars3"></div>
+
+            <div id="parallax">
+                <div className="layer" data-depth="0.6">
+                    <div className="some-space">
+                        <h1>
+                            Welcome to <strong>React Starter Template</strong>
+                        </h1>
+                    </div>
+                </div>
+                <div className="layer" data-depth="0.4">
+                    <div id="particles-js"></div>
+                </div>
+            </div>
+        </React.Fragment>
+    )
 }
 
 export default App
